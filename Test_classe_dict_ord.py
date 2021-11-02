25dae0ac35c8a9840ae52007a1ba901de2703b9e
Richie Ngounou <r.ngounouyossa@technologyandstrategy.com>
2021-11-02 10:41:49 +0100

    no modification from before

--- Test_classe_dict_ord.py
+++ Test_classe_dict_ord.py
@@ -3,35 +3,36 @@
 
 import TP_dict_ordonnee_v2 as dic_ord
 
-#dict1={"jil":0,"emely":8,"pierre":8}
-#dict2= dic_ord.DictionnaireOrdonne()
-#dict3= dic_ord.DictionnaireOrdonne(dict1)
+dict1={"jil":0,"emely":8,"pierre":8}
+dict2= dic_ord.DictionnaireOrdonne()
+dict3= dic_ord.DictionnaireOrdonne(dict1)
 dict4= dic_ord.DictionnaireOrdonne(carotte = 26, haricot = 48)
 
-#print(dict2)
-#print(dict3)
+print(dict2)
+print(dict3)
 print(dict4)
 
-#dict2["pomme"]=6
-#dict2["melon"]=5
-#dict2["poire"]=10
-#dict2["voiture"]=1000
+dict2["pomme"]=6
+dict2["melon"]=5
+dict2["poire"]=10
+dict2["voiture"]=1000
 
-#print(dict2)
+print(dict2)
 
-#del dict2["voiture"]
-#print(dict2)
-
-
+del dict2["voiture"]
+print(dict2)
 
 
-#print(len(dict2))
+print(len(dict2))
 
-#T_F = "melonr" in dict2
-#print(T_F)
+T_F = "melonr" in dict2
+print(T_F)
 
-for cle in dict4:
-    print (cle)
+
+
+
+#for cle in dict4:
+#    print (cle)
 
 
 
