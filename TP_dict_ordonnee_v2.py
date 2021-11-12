95729abaf6aada922622c304309e3c16ea341416
Richie Ngounou <r.ngounouyossa@technologyandstrategy.com>
2021-11-12 18:34:51 +0100

    new version:variable switch to fr to eng; improve sort

--- TP_dict_ordonnee_v2.py
+++ TP_dict_ordonnee_v2.py
@@ -1,65 +1,65 @@
 # python 3.10.0
-import json  # petite triche car import json non vu dans le cours sera utile pour str -> dict()
+import json  
+import typing
 
-# encapsulation des variables mentionnés ne sera pas fait car trop nombreux
 class DictionnaireOrdonne:
     """Classe permmettant de gérer l'utilisation des objets conteneurs"""
 
-    def __init__(self, dictionnaire=dict(), **parametresN_dict):
+    def __init__(self, dictionary:typing.Optional[dict]=dict(), **parameters):
         """Initialisation de objet conteneurs de trois maniere differentes: dict(), dict("p":2,"d":3), dict(dict1)\
             creation de deux listes contenant respectivement la cle et la valeur correspondant de l'objet conteneur"""
 
-        self._liste_cle = list()  # attribut privé
-        self._liste_valeur = list()  # attribut privé
-        self._position = -1  # attr de position pour la méthode next
-        self._items = False
+        self.key = list()  
+        self.value = list()
+        self.position = -1  # attr de position pour la méthode __next__
+        self.item = False
 
-        if (type(dictionnaire) is not dict) and (
-            type(dictionnaire) is not DictionnaireOrdonne
+        if (type(dictionary) is not dict) and (
+            type(dictionary) is not DictionnaireOrdonne
         ):
             raise TypeError(
                 "Cecin'est pas un dictionnaire, veuillez entrer un dictionnaire soit d"
                 "type dict, ou DictionnaireOrdonne"
             )
         else:
-            for cle in dictionnaire:
-                self._liste_cle.append(cle)
-                self._liste_valeur.append(dictionnaire[cle])
+            for cle in dictionary:
+                self.key.append(cle)
+                self.value.append(dictionary[cle])
 
-            for clep, valuep in parametresN_dict.items():
-                self._liste_cle.append(clep)
-                self._liste_valeur.append(valuep)
+            for clep, valuep in parameters.items():
+                self.key.append(clep)
+                self.value.append(valuep)
 
     def __repr__(self):
         """Representation des dictionnaires sur le terminal"""
-        self._string_dict = "{"  # attr privé
-        _nbr_cle = len(self._liste_cle)  # attr privé
+        dictionary_str = "{"  
+        nbr_cle = len(self.key) 
 
-        for i, elem in enumerate(self._liste_cle):
+        for i, elem in enumerate(self.key):
 
-            if i < _nbr_cle - 1:
-                self._string_dict = (
-                    self._string_dict
+            if i < nbr_cle - 1:
+                dictionary_str = (
+                    dictionary_str
                     + '"'
                     + str(elem)
                     + '"'
                     + ":"
-                    + str(self._liste_valeur[i])
+                    + str(self.value[i])
                     + ","
                 )
             else:
-                self._string_dict = (
-                    self._string_dict
+                dictionary_str = (
+                    dictionary_str
                     + '"'
                     + str(elem)
                     + '"'
                     + ":"
-                    + str(self._liste_valeur[i])
+                    + str(self.value[i])
                 )
 
-        self._string_dict = "".join(self._string_dict) + "}" + "\n"
+        dictionary_str = "".join(dictionary_str) + "}" + "\n"
 
-        return "{}".format(self._string_dict)
+        return "{}".format(dictionary_str)
 
     def __str__(self):
         """méthode appelée lorsque qu'un objet est appelée dans print(), elle va appeler __repr__"""
@@ -69,12 +69,12 @@ class DictionnaireOrdonne:
         """méthode appelée lorsqu'on fait objet[index]\
             elle va rediriger vers self._dict_conteneur"""
         try:
-            _found_ind = False  # attr local pas d acces et attr privé
-            for i, cle in enumerate(self._liste_cle):
+            index_found = False  
+            for i, cle in enumerate(self.key):
                 if cle == index:
-                    _found_ind = True
-                    return "{}".format(self._liste_valeur[i])
-            assert _found_ind
+                    index_found = True
+                    return "{}".format(self.value[i])
+            assert index_found
         except AssertionError as ass:
             "Exception:{} occured, this key does not exit in this dictionnary".format(
                 ass
@@ -84,40 +84,40 @@ class DictionnaireOrdonne:
         """méthode appelée lorsqu'on veut stocker une valeur dans un dict special\
             cad: objet[index]=valeur"""
 
-        for i, cle in enumerate(self._liste_cle):
+        for i, cle in enumerate(self.key):
             if cle == index:
-                self._liste_valeur[i] = valeur
+                self.value[i] = valeur
 
-        self._liste_cle.append(index)
-        self._liste_valeur.append(valeur)
+        self.key.append(index)
+        self.value.append(valeur)
 
     def __delitem__(self, index):
         """descrution d'un élément de l'objet conteneur"""
-        _found_ind = False  # attr local pas d'acces et  privé
+        index_found = False  
         try:
-            for i, cle in enumerate(self._liste_cle):
+            for i, cle in enumerate(self.key):
                 if cle == index:
-                    _found_ind = True
-                    del self._liste_cle[i]
-                    del self._liste_valeur[i]
-            assert _found_ind
-        except AssertionError as ass:
+                    index_found = True
+                    del self.key[i]
+                    del self.value[i]
+            assert index_found
+        except AssertionError as a:
             "Exception:{} occured, this key does not exit in this dictionnary, so impossible to delete".format(
-                ass
+                a
             )
 
     def __len__(self):
         """methode permettant d'obtenir la longueur du conteneur"""
-        return len(self._liste_cle)
+        return len(self.key)
 
-    def __contains__(self, objt_c):
+    def __contains__(self, object_container):
         """Méthode renvoyant un booléenne si l'objet cherché est contenu dans l'objet conteneur"""
-        _bool_con = False  # attr privé
-        for i in self._liste_cle:
-            if i == objt_c:
-                _bool_con = True
+        container = False  
+        for i in self.key:
+            if i == object_container:
+                container = True
 
-        return _bool_con
+        return container
 
     def __iter__(self):
         """méthode permettant de créer un objet qui va pouvoir parcourir\
@@ -127,26 +127,26 @@ class DictionnaireOrdonne:
     def __next__(self):
         """méthode qui va permettre a l'objet itérateur de passer d'un \
             élément à l'autre dans l'objet conteneur"""
-        if self._items == False:
-            if self._position == len(self._liste_cle) - 1:
+        if self.item == False:
+            if self.position == len(self.key) - 1:
                 raise StopIteration
-            self._position += 1
+            self.position += 1
 
-            return self._liste_cle[self._position]
+            return self.key[self.position]
         else:
-            if self._position == len(self._liste_cle) - 1:
+            if self.position == len(self.key) - 1:
                 raise StopIteration
-            self._position += 1
+            self.position += 1
 
-            return self._liste_cle[self._position], self._liste_valeur[self._position]
+            return self.key[self.position], self.value[self.position]
 
-    def __add__(self, objt_ajouter=dict()):
+    def __add__(self, object_added:dict()):
         """Additionner des objets conteneurs entre eux: d1= d1 + d2"""
-        _objt_ajouter_dict = json.loads(
-            str(objt_ajouter)
-        )  # petite triche car import json non vu dans le cours
-        if (type(_objt_ajouter_dict) is not dict) and (
-            type(_objt_ajouter_dict) is not DictionnaireOrdonne
+        object_added_type_dict = json.loads(
+            str(object_added)
+        )  
+        if (type(object_added_type_dict) is not dict) and (
+            type(object_added_type_dict) is not DictionnaireOrdonne
         ):
             raise TypeError(
                 "Cecin'est pas un dictionnaire, veuillez entrer un dictionnaire soit d"
@@ -154,9 +154,9 @@ class DictionnaireOrdonne:
             )
         else:
 
-            for cle in dict(_objt_ajouter_dict):
-                self._liste_cle.append(cle)
-                self._liste_valeur.append(_objt_ajouter_dict[cle])
+            for cle in dict(object_added_type_dict):
+                self.key.append(cle)
+                self.value.append(object_added_type_dict[cle])
 
         return self
 
@@ -164,75 +164,73 @@ class DictionnaireOrdonne:
         """Fonction permettant de trier le dictionnaireOrdonnee: aissance de la classe"""
         if reverse == False:
 
-            _tableau_positv_a_sorted = []
-            _tableau_positv_b_sorted = []
-            _tableau_sorted_cle = []
-            _tableau_sorted_valeur = (
-                self._liste_cle
-            )  # none _liste_cle create contracdiction
+            sorted_a = []
+            sorted_b = []
+            sorted_cle = []
+            sorted_value = self.key
+              
 
-            _tableau_sorted_cle = sorted(self._liste_cle)
+            sorted_cle = sorted(self.key)
 
-            for i1, elem1 in enumerate(self._liste_cle):
-                _tableau_positv_b_sorted.append(elem1)
+            for elem1 in self.key:
+                sorted_b.append(elem1)
 
-            for i2, elem2 in enumerate(_tableau_sorted_cle):
-                _tableau_positv_a_sorted.append(elem2)
+            for elem2 in sorted_cle:
+                sorted_a.append(elem2)
 
-            for i3, elem3 in enumerate(_tableau_positv_b_sorted):
+            for i3, elem3 in enumerate(sorted_b):
 
-                for i4, elem4 in enumerate(_tableau_positv_a_sorted):
+                for i4, elem4 in enumerate(sorted_a):
 
                     if elem3 == elem4:
-                        _tableau_sorted_valeur[i4] = self._liste_valeur[i3]
+                        sorted_value[i4] = self.value[i3]
                     else:
                         pass
 
-            self._liste_cle = _tableau_sorted_cle
-            self._liste_valeur = _tableau_sorted_valeur
+            self.key = sorted_cle
+            self.value = sorted_value
 
             return self
 
         elif reverse == True:
 
-            _tableau_positv_a_sorted = []
-            _tableau_positv_b_sorted = []
-            _tableau_sorted_cle = []
-            _tableau_sorted_valeur = self._liste_cle
+            sorted_a = []
+            sorted_b = []
+            sorted_cle = []
+            sorted_value = self.key
 
-            _tableau_sorted_cle = sorted(self._liste_cle, reverse=True)
+            sorted_cle = sorted(self.key, reverse=True)
 
-            for i1, elem1 in enumerate(self._liste_cle):
-                _tableau_positv_b_sorted.append(elem1)
+            for elem1 in self.key:
+                sorted_b.append(elem1)
 
-            for i2, elem2 in enumerate(_tableau_sorted_cle):
-                _tableau_positv_a_sorted.append(elem2)
+            for elem2 in sorted_cle:
+                sorted_a.append(elem2)
 
-            for i3, elem3 in enumerate(_tableau_positv_b_sorted):
+            for i3, elem3 in enumerate(sorted_b):
 
-                for i4, elem4 in enumerate(_tableau_positv_a_sorted):
+                for i4, elem4 in enumerate(sorted_a):
 
                     if elem3 == elem4:
-                        _tableau_sorted_valeur[i4] = self._liste_valeur[i3]
+                        sorted_value[i4] = self.value[i3]
                     else:
                         pass
 
-            self._liste_cle = _tableau_sorted_cle
-            self._liste_valeur = _tableau_sorted_valeur
+            self.key = sorted_cle
+            self.value = sorted_value
 
             return self
 
     def keys(self):
         """Méthode renvoyant une liste des clés de l'objet conteneur appelant"""
-        return self._liste_cle
+        return self.key
 
     def values(self):
         """Méthode renvoyant une liste des valeurs de l'objet conteneur appelant"""
-        return self._liste_valeur
+        return self.value
 
     def items(self):
         """Méthode permettant de renvoyer un tuple contenant la clé et la valeur\
             de l'objet conteneur appelant"""
-        self._items = True
-
+        self.item = True
         return iter(self)
--- Test_classe_dict_ord.py
+++ Test_classe_dict_ord.py
@@ -41,10 +41,10 @@ print(dict2)
 dict2.sort(reverse=True)
 print(dict2)
 
-# still need to define
-print(dict2.keys())  # methode will sent back a lsit of dict2 keys
 
-print(dict2.values())  # methode will sent back a list of dict2 values
+print(dict2.keys())  
+
+print(dict2.values())  
 
 for nom, qtt in dict2.items():
     print("{0} ({1})".format(nom, qtt))
